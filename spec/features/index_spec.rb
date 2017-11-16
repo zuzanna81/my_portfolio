524cecd48103f756d81e7e63efc17fd5bf0f105a
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-16 13:47:55 +0100

    Add projects.yml and content to it, make some small changes in other files

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,7 +1,7 @@
 it 'displays project list' do
     expect(page).to have_css '.projects'
     within '.projects' do
-      expect(page).to have_content 'My Profession'
-      expect(page).to have_content 'When I am not working'
+      expect(page).to have_content 'name'
+      expect(page).to have_content 'description'
     end
   end
